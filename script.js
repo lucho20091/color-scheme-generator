1af5d7cee8923be6794bc8db279278de157b6f69
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2024-05-30 15:28:35 -0500

    yea

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputColor = document.querySelector('#input-color')
+const selection = document.querySelector('#selection')
+const getColorBtn = document.querySelector('#get-color-btn')
+const color1 = document.querySelector('.color1')
+const colors = document.querySelector('.colors')
+
+
+
+
+let dataArr = []
+
+getColorBtn.addEventListener('click', () => {
+    const color = inputColor.value.slice(1,7)
+    const option = selection.value
+    const url = `https://www.thecolorapi.com/scheme?hex=${color}&mode=${option}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const dataColors = data.colors
+            dataArr = []
+            for (const color of dataColors){
+                dataArr.push(color.hex.value)
+            }
+            renderData(dataArr)
+        })
+})
+
+const renderData = (data) => {
+    let html = ''
+    for (let i = 0; i < data.length; i++){
+        html += `
+    <div class="color${i+1}-grid">
+        <div class="color${i+1}"></div>
+        <div class="color-name">${data[i]}</div>
+    </div>`
+    }
+    colors.innerHTML = html
+    for (let i = 0; i < data.length; i++){
+        document.querySelector(`.color${i+1}`).style.backgroundColor = data[i];
+    }
+}
